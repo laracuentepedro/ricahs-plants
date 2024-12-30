aa6b2a35c4b22d5f5ae777bd2ebab2dbf39823b1
Pedro Laracuente <laracuentepedro@gmail.com>
2024-12-30 14:49:14 -0400

    hook up createSession to SignInPage, add error handling for failed sing in

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,28 +1,46 @@
 import FormContainer from "./FormContainer";
 import AuthForm from "./AuthForm";
 import { Link } from "react-router-dom";
-import * as UserService from 'services/user';
+import * as UserService from "services/user";
+import { useState } from "react";
+
 const SignInPage = () => {
+  const [error, setError] = useState(null);
   return (
     <FormContainer>
-        <AuthForm
-          fields={[
-            {
-              label: "username",
-              type: "text",
-            },
-            {
-              label: "password",
-              type: "password",
-            },
-          ]}
-          submitButtonText="sign in"
-          onSubmit={(values)=>UserService.createUser(values)}
-        />
-        <Link 
-        className="text-emerald-700 text-sm underline"
-        
-        to='/sign-up'>sign up</Link>
+      {error && <div className="text-sm text-red-600">{error}</div>}
+
+      <AuthForm
+        fields={[
+          {
+            label: "username",
+            type: "text",
+          },
+          {
+            label: "password",
+            type: "password",
+          },
+        ]}
+        submitButtonText="sign in"
+        onSubmit={async (values) => {
+          const { username, password } = values;
+          const response = await UserService.createSession({
+            username,
+            password,
+          });
+          if (response.status === 201) {
+            setError(null);
+            console.log("sign in successful");
+          } else {
+            const data = await response.json();
+            setError(data.error);
+            console.log(data.error);
+          }
+        }}
+      />
+      <Link className="text-emerald-700 text-sm underline" to="/sign-up">
+        sign up
+      </Link>
     </FormContainer>
   );
 };
