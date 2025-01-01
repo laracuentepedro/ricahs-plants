a2f8438d698e644bae19d751d8586c7e1162cde3
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-01 13:08:30 -0400

    add plant fetching functionality to PlantListPage and create plant service

--- src/pages/PlantListPage/index.jsx
+++ src/pages/PlantListPage/index.jsx
@@ -1,6 +1,17 @@
 import NavBar from "shared-components/NavBar";
 import RedirectToSignInIfSignedOut from "shared-components/RedirectToSignInIfSignedOut";
+import * as plantService from "services/plant";
+import { useEffect } from "react";
 const PlantListPage = () => {
+  useEffect(() => {
+    const fetchPlants = async () => {
+        const response = await plantService.getPlants();
+        console.log(response.status);
+        const data = await response.json();
+        console.log(data);
+    }
+    fetchPlants();
+  }, []);
   return (
     <RedirectToSignInIfSignedOut>
       <div>
