6de6a88c6f8586c32470921bcb9189919cc5a748
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-01 13:38:08 -0400

    add loading state to PlantListPage for improved user experience during plant fetching

--- src/pages/PlantListPage/index.jsx
+++ src/pages/PlantListPage/index.jsx
@@ -1,22 +1,26 @@
 import NavBar from "shared-components/NavBar";
 import RedirectToSignInIfSignedOut from "shared-components/RedirectToSignInIfSignedOut";
 import * as plantService from "services/plant";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const PlantListPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchPlants = async () => {
-        const response = await plantService.getPlants();
-        console.log(response.status);
-        const data = await response.json();
-        console.log(data);
-    }
+        setIsLoading(true);
+      const response = await plantService.getPlants();
+      console.log(response.status);
+      const data = await response.json();
+      console.log(data);
+      setIsLoading(false);
+    };
     fetchPlants();
   }, []);
   return (
     <RedirectToSignInIfSignedOut>
-      <div>
-        <NavBar />
-      </div>
+      <NavBar />
+      {isLoading ? <div className="pt-40 flex justify-center">
+        <i className="fa-solid fa-circle-notch animate-spin text-3xl text-emerald-700"></i>
+      </div> : <div>plants in stock</div>}
     </RedirectToSignInIfSignedOut>
   );
 };
