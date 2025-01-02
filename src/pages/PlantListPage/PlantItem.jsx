e2af5b257b0319829d4ddbcf8f7a553d71ca8c67
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-02 08:45:06 -0400

    enhance PlantItem component to display a random plant image and its pot color

--- src/pages/PlantListPage/PlantItem.jsx
+++ src/pages/PlantListPage/PlantItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-
+import { useState } from 'react';
 const POT_COLORS = {
     stone: "bg-stone-200",
     slate: "bg-slate-300",
@@ -10,24 +10,28 @@ const POT_COLORS = {
     amber: "bg-amber-600",
   };
 
+  const getRandomIndex = (array) => Math.floor(Math.random()*array.length);
+
 const PlantItem = (props) => {
   const { name, price, images } = props.plant;
+    const [selectedPlant, setSelectedPlant] = useState(getRandomIndex(images));
+  
   return (
     <div className="p-4">
-      <img className="w-60 h-90 object-cover rounded-lg" src={images[0]["src"]} />
+      <img className="w-60 h-90 object-cover rounded-lg" src={images[selectedPlant]["src"]} />
       <div className="flex items-center justify-between font-lato text-emerald-700 mt-2">
         <div className="font-playfair text-xl">{name}</div>
         <div>${price}</div>
       </div>
       <div className='flex justify-between items-center'>
-        <div className="text-slate-500 text-sm">{images[0]['pot_color']}</div>
+        <div className="text-slate-500 text-sm">{images[selectedPlant]['pot_color']}</div>
         <div className='flex mt-4'>
             {
                 images.map((image, idx) =>(
                     <div 
                     key={idx}
                     className={clsx(
-                        'h-4 w-4 rounded-full border ml-1', POT_COLORS[image.pot_color]
+                        'h-4 w-4 rounded-full border border-slate-300 ml-1', POT_COLORS[image.pot_color]
                     )}></div>
                 ) )
             }
