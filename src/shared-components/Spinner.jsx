4c7e41ab71059dfdd87cb7ac621a58cf4b90431c
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-03 20:40:24 -0400

    add Spinner component for loading indication

---
+++ src/shared-components/Spinner.jsx
@@ -0,0 +1,6 @@
+const Spinner = () => {
+    return <div className=" flex justify-center items-center bg-emerald-50 h-screen">
+    <i className="fa-solid fa-circle-notch animate-spin text-3xl text-emerald-700"></i>
+  </div>
+}
+export default Spinner;
