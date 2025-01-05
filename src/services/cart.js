1cc51ecc85556a40f0a89085c44ce5e21b26e7be
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-05 09:31:24 -0400

    refactor addToCart function to use destructured parameters for improved readability

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,7 +1,7 @@
 import apiFetch from "./apiFetch";
 
-export const addToCart = (quantity, pot_color, plantId) =>
-    apiFetch("POST", `cart/plants/${plantId}`, {
+export const addToCart = ({plantId, quantity, potColor}) =>
+    apiFetch("POST", `/cart/plants/${plantId}`, {
         quantity: quantity,
-        pot_color: pot_color
+        pot_color: potColor
     })
