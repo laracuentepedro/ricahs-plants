3a6d4a8ea7e42d085869d09b1c206bd4fef4376c
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-06 07:45:12 -0400

    add cart button to NavBar and improve user menu layout

--- src/shared-components/NavBar/index.jsx
+++ src/shared-components/NavBar/index.jsx
@@ -9,33 +9,45 @@ const NavBar = () => {
       <div className="w-full max-w-4xl px-4 py-4 text-white flex items-center justify-between">
         <div className="flex items-center">
           <Link to="/plants">
-          <img
-            className="w-12 mr-2"
-            src="https://static-task-assets.react-formula.com/capstone_logo_light.png"
+            <img
+              className="w-12 mr-2"
+              src="https://static-task-assets.react-formula.com/capstone_logo_light.png"
             />
-            </Link>
+          </Link>
           <div className="text-2xl font-playfair">Rica&apos;s Plants</div>
         </div>
-        <div className="relative">
-          <button
-            onClick={() => setUserMenuOpen(!userMenuOpen)}
+        <div className="flex gap-8">
+          <div className="relative">
+            <button
+              onClick={() => setUserMenuOpen(!userMenuOpen)}
+              className="font-lato flex items-center text-emerald-100"
+            >
+              <i className="fa-solid fa-user mr-2 text-xl"></i>
+              {username}
+            </button>
+            {userMenuOpen && (
+              <div className="absolute w-28 flex items-center justify-center py-2 right-0 top-8 border bg-white text-slate-500 text-sm  shadow-sm rounded-lg">
+                <button
+                  onClick={() => {
+                    setUserMenuOpen(false);
+                    signOut();
+                  }}
+                >
+                  <i className="fa-solid fa-arrow-right-from-bracket mr-2"></i>
+                  sign out
+                </button>
+              </div>
+            )}
+          </div>
+          <div
+            onClick={() => null}
             className="font-lato flex items-center text-emerald-100"
           >
-            <i className="fa-solid fa-user mr-2 text-xl"></i>
-            {username}
-          </button>
-          {userMenuOpen && (
-            <div className="absolute w-28 flex items-center justify-center py-2 right-0 top-8 border bg-white text-slate-500 text-sm  shadow-sm rounded-lg">
-              <button onClick={()=>{
-                setUserMenuOpen(false);
-                signOut()
-              }
-            }>
-                <i className="fa-solid fa-arrow-right-from-bracket mr-2"></i>
-                sign out
-              </button>
-            </div>
-          )}
+            <button>
+              <i className="fa-solid fa-cart-shopping mr-2"></i>
+              cart
+            </button>
+          </div>
         </div>
       </div>
     </nav>
