ccf5d591b3802d1cb4d2d411cdf313571d240efb
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-07 16:56:35 -0400

    refactor CartModal to improve layout and add item summary display

--- src/shared-components/NavBar/modals/CartModal/index.jsx
+++ src/shared-components/NavBar/modals/CartModal/index.jsx
@@ -2,7 +2,7 @@ import SessionContext from "contexts/SessionContext";
 import { useCallback, useContext } from "react";
 import { RemoveScroll } from "react-remove-scroll";
 import { useState, useEffect } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { getCart } from "services/cart";
 import Spinner from "shared-components/Spinner";
 import CartItem from "./CartItem";
@@ -11,8 +11,8 @@ const CartModal = ({ onClickClose }) => {
   const { username } = useContext(SessionContext);
   const [cart, setCart] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
-  const fetchCart = useCallback (async () => {
+
+  const fetchCart = useCallback(async () => {
     setIsLoading(true);
     const response = await getCart();
     const data = await response.json();
@@ -23,10 +23,18 @@ const CartModal = ({ onClickClose }) => {
   useEffect(() => {
     fetchCart();
   }, [fetchCart]);
+
+  let totalItems = 0;
+  let subtotal = 0;
+  for (let i = 0; i < cart.length; i += 1) {
+    totalItems += cart[i].quantity;
+    subtotal += cart[i].quantity * cart[i].price_per_unit;
+  }
+
   return (
     <RemoveScroll>
       <div className="z-50 fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-end">
-        <div className="bg-emerald-50 w-full max-w-lg">
+        <div className="bg-emerald-50 w-full max-w-lg flex flex-col h-screen">
           <div
             className="
             flex justify-center py-8 bg-emerald-800 text-emerald-50 font-playfair text-2xl"
@@ -39,27 +47,41 @@ const CartModal = ({ onClickClose }) => {
           >
             <i className="fa-solid fa-xmark"></i>
           </button>
-          {isLoading ? <Spinner /> 
-          : 
-          <div className="min-h-screen mt-4">
-            {cart.map((item,idx)=>(
-                <>
-                {idx > 0 && <div className="h-px bg-slate-200 mx-4"></div>}
-                <CartItem 
-                key={item.id} 
-                item={item}
-                fetchCart={fetchCart}
-                />
-                </>
-            ))}
-            </div>}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className="flex-1 overflow-y-scroll">
+                {cart.map((item, idx) => (
+                  <>
+                    {idx > 0 && <div className="h-px bg-slate-200 mx-4"></div>}
+                    <CartItem key={item.id} item={item} fetchCart={fetchCart} />
+                  </>
+                ))}
+              </div>
+              <div className="flex flex-col text-slate-500 font-lato pb-8 border-t p-4 border-slate-200">
+                <div className="flex justify-between">
+                  <div>{totalItems} items</div>
+                  <div>subtotal: {subtotal}</div>
+                </div>
+                <button
+                  onClick={() =>
+                    alert("Checkout functionality is being developed")
+                  }
+                  className="bg-emerald-700 text-white py-2 rounded-full mt-4 flex items-center justify-center"
+                >
+                  Checkout <i className="ml-2 fa-solid fa-right-long"></i>
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </RemoveScroll>
   );
 };
 CartModal.propTypes = {
-  onClickClose: PropTypes.func.isRequired
+  onClickClose: PropTypes.func.isRequired,
 };
 
 export default CartModal;
