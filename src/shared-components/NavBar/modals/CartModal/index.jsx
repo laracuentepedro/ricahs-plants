9eacd4be1206945da607150d25ec76a97f56c07c
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-07 22:55:37 -0400

    feat: add framer-motion for slide-in animation in CartModal

--- src/shared-components/NavBar/modals/CartModal/index.jsx
+++ src/shared-components/NavBar/modals/CartModal/index.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import { getCart } from "services/cart";
 import Spinner from "shared-components/Spinner";
 import CartItem from "./CartItem";
+import { motion } from "framer-motion";
 
 const CartModal = ({ onClickClose }) => {
   const { username } = useContext(SessionContext);
@@ -19,6 +20,7 @@ const CartModal = ({ onClickClose }) => {
     setCart(data);
     setIsLoading(false);
   }, []);
+
   useEffect(() => {
     fetchCart();
   }, [fetchCart]);
@@ -31,43 +33,45 @@ const CartModal = ({ onClickClose }) => {
   }
 
   return (
-      <div className="bg-emerald-50 w-full max-w-lg flex flex-col h-screen font-lato">
-        <div
-          className="
+    <motion.div 
+    initial={{ translateX: "100%" }} 
+    animate={{ translateX: 0 }} 
+    transition={{ duration: 0.5 }}
+    className="bg-emerald-50 w-full max-w-lg flex flex-col h-screen font-lato">
+      <div
+        className="
             flex justify-center py-8 bg-emerald-800 text-emerald-50 font-playfair text-2xl"
-        >
-          {username}&apos;s Cart
-        </div>
-
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <>
-            <div className="flex-1 overflow-y-scroll">
-              {cart.map((item, idx) => (
-                <>
-                  {idx > 0 && <div className="h-px bg-slate-200 mx-4"></div>}
-                  <CartItem key={item.id} item={item} fetchCart={fetchCart} />
-                </>
-              ))}
-            </div>
-            <div className="flex flex-col text-slate-500 font-lato pb-8 border-t p-4 border-slate-200">
-              <div className="flex justify-between">
-                <div>{totalItems} items</div>
-                <div>subtotal: ${subtotal}</div>
-              </div>
-              <button
-                onClick={() =>
-                  alert("Checkout functionality is being developed")
-                }
-                className="bg-emerald-700 text-white py-2 rounded-full mt-4 flex items-center justify-center"
-              >
-                Checkout <i className="ml-2 fa-solid fa-right-long"></i>
-              </button>
-            </div>
-          </>
-        )}
+      >
+        {username}&apos;s Cart
       </div>
+
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="flex-1 overflow-y-scroll">
+            {cart.map((item, idx) => (
+              <>
+                {idx > 0 && <div className="h-px bg-slate-200 mx-4"></div>}
+                <CartItem key={item.id} item={item} fetchCart={fetchCart} />
+              </>
+            ))}
+          </div>
+          <div className="flex flex-col text-slate-500 font-lato pb-8 border-t p-4 border-slate-200">
+            <div className="flex justify-between">
+              <div>{totalItems} items</div>
+              <div>subtotal: ${subtotal}</div>
+            </div>
+            <button
+              onClick={() => alert("Checkout functionality is being developed")}
+              className="bg-emerald-700 text-white py-2 rounded-full mt-4 flex items-center justify-center"
+            >
+              Checkout <i className="ml-2 fa-solid fa-right-long"></i>
+            </button>
+          </div>
+        </>
+      )}
+    </motion.div>
   );
 };
 CartModal.propTypes = {
