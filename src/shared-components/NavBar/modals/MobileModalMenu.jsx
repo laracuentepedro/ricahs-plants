f4f3cd9d4cd7425250839e00afaaea4bb0977ae9
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-07 22:59:11 -0400

    feat: add framer-motion for slide-in animation in MobileModalMenu

--- src/shared-components/NavBar/modals/MobileModalMenu.jsx
+++ src/shared-components/NavBar/modals/MobileModalMenu.jsx
@@ -1,12 +1,17 @@
 import SessionContext from "contexts/SessionContext";
 import { useContext } from "react";
 import PropTypes from 'prop-types';
+import { motion } from "framer-motion";
 
 const MobileModalMenu = (props) => {
   const { onCartOpenClick } = props;
   const { username, signOut } = useContext(SessionContext);
   return (
-    <div className="bg-emerald-800 pt-12 pr-16 pb-8 rounded-bl-lg flex flex-col items-start text-lg">
+    <motion.div 
+    initial={{translateY: "-100%"}}
+    animate={{translateY: 0}}
+    transition={{duration: 0.5}}
+    className="bg-emerald-800 pt-12 pr-16 pb-8 rounded-bl-lg flex flex-col items-start text-lg">
       <div className="px-6 py-4">
         <i className="fa-solid fa-user mr-1"></i>
         {username}
@@ -20,7 +25,7 @@ const MobileModalMenu = (props) => {
       >
         <i className="fa-solid fa-shopping-cart mr-1"></i>cart
       </button>
-    </div>
+    </motion.div>
   );
 };
 MobileModalMenu.propTypes = {
