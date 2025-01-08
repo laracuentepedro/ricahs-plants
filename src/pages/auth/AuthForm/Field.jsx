1d56eea31a45bb53218fa279ff5842dcc8ab415d
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-08 09:11:31 -0400

    fix: add padding to input field in AuthForm for better usability

--- src/pages/auth/AuthForm/Field.jsx
+++ src/pages/auth/AuthForm/Field.jsx
@@ -6,7 +6,7 @@ const Field = (props) => {
     <div className="font-lato flex flex-col w-full text-slate-400">
       <label htmlFor={field.label}>{field.label}</label>
       <input
-        className="border border-slate-200 bg-slate-50 py-1 my-4 rounded-lg focus:outline-emerald-300"
+        className="border border-slate-200 bg-slate-50 py-1 px-2 my-4 rounded-lg focus:outline-emerald-300"
         value={value}
         onChange={onChange}
         type={field.type}
