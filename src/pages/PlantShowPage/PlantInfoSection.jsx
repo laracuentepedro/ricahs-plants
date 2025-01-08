ed88c72a861c733cbd39e0c30a191543ef4da346
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-07 22:49:08 -0400

    feat: add framer-motion and react-medium-image-zoom for enhanced image interactions in PlantInfoSection

--- src/pages/PlantShowPage/PlantInfoSection.jsx
+++ src/pages/PlantShowPage/PlantInfoSection.jsx
@@ -4,10 +4,14 @@ import BenefitBox from "./BenefitBox";
 import PlantPurchaseOptions from "./PlantPurchaseOptions";
 import { useState } from "react";
 import { getRandomIndex } from "utils";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const PlantInfoSection = (props) => {
   const { plant } = props;
-  const [selectedPlant, setSelectedPlant] = useState(getRandomIndex(plant.images))
+  const [selectedPlant, setSelectedPlant] = useState(
+    getRandomIndex(plant.images)
+  );
 
   return (
     <div className="flex flex-col md:flex-row pt-24 px-4">
@@ -15,19 +19,21 @@ const PlantInfoSection = (props) => {
         <PlantHeading plant={plant} />
       </div>
       <div className="flex-1">
-        <img className="rounded-lg" src={plant.images[selectedPlant].src} />
+        <Zoom>
+          <img className="rounded-lg" src={plant.images[selectedPlant].src} />
+        </Zoom>
         <div className="flex mt-4 mb-8">
-            <BenefitBox
-                icon="fa-regular fa-circle-check"
-                title="Guaranteed Healthy"
-                description="Guaranteed to arrive healthy or your money back"
-            />
-            <div className="w-px bg-slate-300 mx-4"></div>
-            <BenefitBox
-                icon="fa-solid fa-truck-fast"
-                title="Free Shipping"
-                description="Get free ground shippinng on orders of $50 or more"
-            />
+          <BenefitBox
+            icon="fa-regular fa-circle-check"
+            title="Guaranteed Healthy"
+            description="Guaranteed to arrive healthy or your money back"
+          />
+          <div className="w-px bg-slate-300 mx-4"></div>
+          <BenefitBox
+            icon="fa-solid fa-truck-fast"
+            title="Free Shipping"
+            description="Get free ground shippinng on orders of $50 or more"
+          />
         </div>
       </div>
       <div className="flex flex-col flex-1 md:px-8">
@@ -38,9 +44,10 @@ const PlantInfoSection = (props) => {
           {plant.description}
         </p>
         <PlantPurchaseOptions
-        selectedPlant={selectedPlant} 
-        setSelectedPlant={setSelectedPlant}
-        images={plant.images}/>
+          selectedPlant={selectedPlant}
+          setSelectedPlant={setSelectedPlant}
+          images={plant.images}
+        />
       </div>
     </div>
   );
